3925f7ab2c1f9f5b1ff1dd983bf9850adaaab153
kacper <k4cper.petelicki@gmail.com>
2024-03-16 16:35:31 +0100

    add

--- puzzle/src/components/overlay/Overlay.js
+++ puzzle/src/components/overlay/Overlay.js
@@ -1,11 +1,8 @@
 import './Overlay.css'
 
-const images =[
-  "black", "green" ,"pink","red","yellow","white","purple","grey","orange"
-,"red","red","red","red","red","red","red"
-];
+
 const Overlay = () =>
     new Array(16)
         .fill()
-        .map((_,i) => <div key={i} className="overlay"  style={{ backgroundColor: images[i] }}/>)
+        .map((_,i) => <div key={i} className="overlay"/>)
 export default Overlay;
