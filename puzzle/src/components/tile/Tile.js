2d7e27e6f60fc6bed927f6954984e6256847d937
kacper <k4cper.petelicki@gmail.com>
2024-03-18 00:46:35 +0100

    finished sass

--- puzzle/src/components/tile/Tile.js
+++ puzzle/src/components/tile/Tile.js
@@ -1,10 +1,45 @@
-import React from 'react'
-import './Tile.scss'
+// Tile.js
+import React from 'react';
+import './Tile.scss';
 
-const Tile=({number,moveTile,size})=>
-    <div
-        onClick={()=> moveTile(number)}
-        className={`number ${number.value===number.index+1  ? 'correct' : ''} ${number.value===(size*size/size) ? 'disabled' : ''} slot--${number.index}`}>
-            {number.value=== size? '': number.value}
-        </div>
-    export default Tile
+// Define generateCellStyles function
+function generateCellStyles(rows) {
+  let styles = '';
+
+  const columns = rows;
+  const size = 100;
+
+  for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < columns; i++) {
+      styles += `
+        .number.slot--${j * rows + i} {
+          left: calc(${i} * ${size}px);
+          top: calc(${j} * ${size}px);
+        }
+      `;
+    }
+  }
+
+  return styles;
+}
+
+const Tile = ({ number, moveTile, size }) => {
+  const sas = Math.sqrt(size);
+  const cellStyles = generateCellStyles(sas);
+
+  return (
+    <div>
+      <style>{cellStyles}</style>
+
+      <div
+        onClick={() => moveTile(number)}
+        className={`number ${number.value === number.index + 1 ? 'correct' : ''} ${
+          number.value === size * size / size ? 'disabled' : ''
+        } slot--${number.index}`}>
+        {number.value === size ? '' : number.value}
+      </div>
+    </div>
+  );
+};
+
+export default Tile;
